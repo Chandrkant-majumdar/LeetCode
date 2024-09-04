9e643924096ea2beb860526f54cc29eb6023ec6e
Chandrkant-majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-09-04 10:30:24 +0530

    Time: 22 ms (66.39%), Space: 29.2 MB (92.92%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -12,20 +12,53 @@
 class Solution {
 public:
 
-    void f(TreeNode *root,int &cnt){
+    // void f(TreeNode *root,int &cnt){
            
-           if(!root) return;
+    //        if(!root) return;
            
-           f(root->left,cnt);
-           cnt++;
-           f(root->right,cnt);
-        //    return cnt;
+    //        f(root->left,cnt);
+    //        cnt++;
+    //        f(root->right,cnt);
+    //     //    return cnt;
+    // }
+    int f(TreeNode *root){
+        if(!root) return 0;
+        int l=lh(root);
+        int r=rh(root);
+
+        if(l==r){
+            return (1<<l)-1;
+        }
+
+        
+        return 1+f(root->left)+f(root->right);
+
+    }
+    int lh(TreeNode *root){
+        // if(!root) return 0;
+         int c=0;
+        while(root){
+            c++;
+            root=root->left;
+        }
+        return c;
+
+    }
+    int rh(TreeNode *root){
+        // if(!root) return 0;
+         int c=0;
+        while(root){
+            c++;
+            root=root->right;
+        }
+        return c;
+
     }
     int countNodes(TreeNode* root) {
-        if(!root) return 0;
-        int cnt=0;
-     f(root,cnt);
-     return cnt;
+        // if(!root) return 0;
+        // int cnt=0;
+     return f(root);
+    //  return cnt;
         
 
     }
