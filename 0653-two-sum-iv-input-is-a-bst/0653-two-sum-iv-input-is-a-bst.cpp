f442de9e06e7448c67ae2192407abdbf32bad8af
Chandrkant-majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-09-07 07:25:41 +0530

    Time: 30 ms (50.93%), Space: 37.3 MB (37.08%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,8 +9,47 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTItr{
+
+    public:
+    stack<TreeNode *> s;
+    bool rev=true;
+    BSTItr(TreeNode *root,bool isRev){
+        rev=isRev;
+        pushAll(root);
+    }
+    void pushAll(TreeNode *root){
+           while(root){
+            s.push(root);
+            if(rev){
+                root=root->right;
+            }else{
+                root=root->left;
+            }
+           }
+    }
+
+    int next(){
+        TreeNode *t=s.top();
+        s.pop();
+        if(rev){
+            pushAll(t->left);
+        }else{
+            pushAll(t->right);
+        }
+        return t->val;
+    }
+    bool hasNext(){
+        return !s.empty();
+    }
+
+
+};
 class Solution {
 public:
+
+  //    Method 1
+  /*
     void f(TreeNode *root,vector<int>&ans){
         if(!root) return;
         f(root->left,ans);
@@ -32,4 +71,34 @@ public:
         }
         return false;
     }
+    */
+    //   Method 2
+
+    /*------------------------------------------------------*/
+
+     bool findTarget(TreeNode* root, int k) {
+
+    if(!root) return false;
+
+    BSTItr l(root,false);
+    BSTItr r(root,true);
+
+    int i=l.next();
+    int j=r.next();
+    
+    while(i<j){
+        int sum=(i+j);
+        if(sum==k) return true;
+        else if(sum<k){
+            i=l.next();
+            
+        }else{
+            j=r.next();
+        }
+    }
+       
+       return false;
+
+     }
+
 };
