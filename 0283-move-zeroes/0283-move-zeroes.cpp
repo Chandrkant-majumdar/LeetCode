012892c29ac5c42026cdf3efe10402293d7b9b1e
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-09-30 00:06:36 +0530

    Time: 12 ms (80.37%), Space: 21.9 MB (27.75%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i=0;
+        for(auto x:nums){
+            if(x!=0){
+                nums[i++]=x;
+            }
+        }
+        while(i<nums.size()){
+            nums[i++]=0;
+        }
+    }
+};
