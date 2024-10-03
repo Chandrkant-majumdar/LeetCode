6d5c3cfd7f8eb34011b61844179307f0f6acdddd
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-10-03 22:55:56 +0530

    Time: 0 ms (100%), Space: 9.1 MB (10.86%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& ma) {
+    void rotate1(vector<vector<int>>& ma) {
         int n=ma.size();
         int m=ma[0].size();
         for(int i=0;i<n;i++){
@@ -13,5 +13,24 @@ public:
         for(int i=0;i<n;i++){
             reverse(ma[i].begin(),ma[i].end());
         }
+    }
+    void rotate(vector<vector<int>>& ma) {
+        int n=ma.size();
+        int m=ma[0].size();
+        vector<vector<int>>res(m,vector<int>(n));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                res[j][n-i-1]=ma[i][j];
+            }
+        }
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+               ma[i][j]=res[i][j];
+            }
+            // cout<<endl;
+        }
+        // for(int i=0;i<n;i++){
+        //     reverse(ma[i].begin(),ma[i].end());
+        // }
     }
 };
