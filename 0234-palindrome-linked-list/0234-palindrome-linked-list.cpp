ccb36590465f51bfb543b14d56710d592c2f61d5
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-10-28 17:09:50 +0530

    Time: 4 ms (54.19%), Space: 116.9 MB (64.79%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,7 +10,39 @@
  */
 class Solution {
 public:
+    
+    ListNode* rev(ListNode *head){
+        ListNode *p=NULL,*cur=head,*front;
+        while(cur!=nullptr){
+            front=cur->next;
+            cur->next=p;
+            p=cur;
+            cur=front;
+            
+        }
+        return p;
+    }
+   
     bool isPalindrome(ListNode* head) {
+      ListNode *slow=head,*fast=head,*t1;
+      while(fast->next!=nullptr && fast->next->next!=nullptr){
+        slow=slow->next;
+        fast=fast->next->next;
+      }
+    //   cout<<slow->val<<" "<<slow->next->val<<endl;
+      t1=rev(slow->next);
+      slow->next=nullptr;
+      slow=head;
+      while(t1!=nullptr && slow!=nullptr ){
+        if(t1->val!=slow->val) return false;
+        t1=t1->next;
+        slow=slow->next;
+      }
+            // cout<<t1->next->val<<endl;
+
+       return true;
+    }
+    bool isPalindrome1(ListNode* head) {
         ListNode *t=head;
         stack<int>s;
         while(t!=nullptr){
