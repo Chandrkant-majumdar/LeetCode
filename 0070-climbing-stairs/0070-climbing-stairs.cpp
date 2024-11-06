2e46be9566b7f5e228d22e232a99450738c18249
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-07 00:07:28 +0530

    Time: 0 ms (100%), Space: 7.7 MB (35.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int f(int i,vector<int>&dp){
+
         if(i==0) return 1;
         if(i<0) return 0;
-     if(dp[i]!=-1) return dp[i];
-        int tk=f(i-1,dp);
-        int nt=f(i-2,dp);
-        return dp[i]=(tk+nt);
+        if(dp[i]!=-1) return dp[i];
+        int tk1=f(i-1,dp);
+        int  tk2=f(i-2,dp);
+        return dp[i]=tk1+tk2;
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
