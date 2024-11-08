9779dce87a80ec83e92b97c275ca1ce3e6ab0286
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-08 22:06:57 +0530

    Time: 4 ms (19.42%), Space: 15 MB (5.33%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,50 +1,29 @@
 class Solution {
 public:
-    int f(int i,int j,vector<vector<int>>&ma,vector<vector<int>>&dp){
-        int n=ma.size();
-        int m=ma[0].size();
-        if(j<0 || j>=m){
-            return 1e9;
-        }
-        if(i==0){
-            return ma[i][j];
-        } 
-       if(dp[i][j]!=-1) return dp[i][j];
-        int up=ma[i][j]+f(i-1,j,ma,dp);
-        int ld=ma[i][j]+f(i-1,j-1,ma,dp);
-        int rd=ma[i][j]+f(i-1,j+1,ma,dp);
+    int f(int i,int j,vector<vector<int>>& dp,vector<vector<int>>& ma){
+       int n=ma.size();
+       int m=ma[0].size();
+       if(i<0 || i>=n || j<0 || j>=m) return 1e9;
 
-        return dp[i][j]=min(up,min(ld,rd));
-
-
+       if(i==n-1){
+        return ma[i][j];
+       }
+       if(dp[i][j]!=-2) return dp[i][j];
+        int down=ma[i][j]+f(i+1,j,dp,ma);
+        int ld=ma[i][j]+f(i+1,j-1,dp,ma);
+        int rd=ma[i][j]+f(i+1,j+1,dp,ma);
+        // int down=g[i][j]+f(i+1,j,dp,ma);
+        return dp[i][j]=min({down,ld,rd});
+        
+        
     }
     int minFallingPathSum(vector<vector<int>>& ma) {
         int n=ma.size();
-        int m=ma[0].size();
-         vector<vector<int>>dp(n,vector<int>(m,0));
-        // int ans=1e9;
-        // for(int i=0;i<m;i++){
-        //    int t=f(n-1,i,ma,dp);
-        //     ans=min(ans,t);
-        // }
-        // return ans;
-        for(int i=0;i<m;i++){
-            dp[0][i]=ma[0][i];
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<m;j++){
-                //  if(dp[i][j]!=-1) return dp[i][j];
-                    int up=ma[i][j]+dp[i-1][j];
-                    int ld=(j-1>=0)?ma[i][j]+dp[i-1][j-1]:1e9;
-                    int rd=(j+1<m)?ma[i][j]+dp[i-1][j+1]:1e9;
-
-              dp[i][j]=min(up,min(ld,rd));
-            }
-        }
+       int m=ma[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,-2));
         int ans=1e9;
         for(int i=0;i<m;i++){
-           int t=dp[n-1][i];
-            ans=min(ans,t);
+            ans=min(ans,f(0,i,dp,ma));
         }
         return ans;
     }
