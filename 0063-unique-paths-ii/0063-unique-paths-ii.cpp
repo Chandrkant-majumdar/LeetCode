bf8366f226dab9bd0c99c4616a201b769b37b01c
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-08 21:27:41 +0530

    Time: 0 ms (100%), Space: 10.6 MB (7.53%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,18 +1,55 @@
 class Solution {
 public:
-   int f(int i,int j,int n,int m,vector<vector<int>>&dp,vector<vector<int>>& o){
-
-        if(i>=n || j>=m || o[i][j]==1) return 0;
+     
+     int f(int i,int j,vector<vector<int>>& dp,vector<vector<int>>& g){
+         int n=g.size(),m=g[0].size();
+        if(i>=n || j>=m || g[i][j]==1) return 0; 
         if(i==n-1 && j==m-1) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
-        int d=f(i+1,j,n,m,dp,o);
-        int r=f(i,j+1,n,m,dp,o);
-        return dp[i][j]=d+r;
-    }
-    int uniquePathsWithObstacles(vector<vector<int>>& o) {
-        int n=o.size();
-        int m=o[0].size();
+        int down=f(i+1,j,dp,g);
+        int right=f(i,j+1,dp,g);
+
+        return dp[i][j]=down+right;
+
+     }
+    int uniquePathsWithObstacles(vector<vector<int>>& g) {
+        int n=g.size(),m=g[0].size();
+        //  if(n==1 && m==1) return 1;
+        // if(n==1) return 0;
+       
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(0,0,n,m,dp,o);
+        return f(0,0,dp,g);
+    }
+    int uniquePathsWithObstacles1(vector<vector<int>>& g) {
+        int n=g.size(),m=g[0].size();
+        //  if(n==1 && m==1) return 1;
+        // if(n==1) return 0;
+       
+        vector<vector<int>>dp(n,vector<int>(m,0));
+        // if(g[n-1][m-1]==1)
+        // dp[n-1][m-1]=0;
+        // else
+        if(g[0][0]==1 || g[n-1][m-1]==1) return 0;
+        dp[n-1][m-1]=1;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(g[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i==n-1 && j==m-1) continue;
+                //  (i+1<n)
+                // if(g[i][j]==1){
+                //     dp[i][j]=0;
+                //     continue;
+                // }
+                 int down= (i+1<n)?dp[i+1][j]:0;
+
+                  int right= (j+1<m)?dp[i][j+1]:0;
+
+                  dp[i][j]=down+right;
+            }
+        }
+        return dp[0][0];
     }
 };
