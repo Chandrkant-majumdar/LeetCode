36d71d8198f3f3ff8c9cc03f4dfb2924a37f0a12
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-09 12:49:03 +0530

    Time: 19 ms (64.25%), Space: 27 MB (51.5%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int f(int i,int A,vector<int>& c,vector<vector<int>>&dp){
-         
-         if(i==0){
-            if(A%c[i]==0) return 1;
-            else
-            return 0;
-         }
-        if(dp[i][A]!=-1) return dp[i][A];
-        int tk=(A>=c[i])?f(i,A-c[i],c,dp):0;
-        int nt=f(i-1,A,c,dp);
-
-        return dp[i][A]=(tk+nt);
+   int f(int i,int a, vector<vector<int>>&dp,vector<int>& c){
+    
+    
+    if(i==0){ 
+           
+            if(a==0 || a==c[i]) return 1;
+            
+            if(a%c[i]==0) return 1;
+        
+      
+        return 0;
+        
     }
-    int change(int A, vector<int>& c) {
-        int n=c.size();
-        vector<vector<int>>dp(n,vector<int>(A+1,-1));
-        return f(n-1,A,c,dp);
+    if(dp[i][a]!=-1) return dp[i][a];
+    int tk=(a>=c[i])?f(i,a-c[i],dp,c):0;
+    int nt=f(i-1,a,dp,c);
+    return dp[i][a]=tk+nt;
+   }
+    int change(int sum,vector<int>& arr) {
+         int n=arr.size();
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        int ans=f(n-1,sum,dp,arr);
+        return ans==1e9?-1:ans;
     }
 };
