31c087a0937527fb20711abbbb581d696ef2fcad
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-09 11:30:26 +0530

    Time: 151 ms (67.49%), Space: 98.9 MB (45.69%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    bool f(int i,int sum,vector<int>&nums,vector<vector<int>>&dp){
-        if(sum==0) return true;
+bool f(int i,int sum,vector<int>& arr,vector<vector<int>>&dp){
+        if(sum==0) return 1;
         if(i==0){
-            return nums[i]==sum;
+            return (sum==arr[i]);
+            
         }
+        
         if(dp[i][sum]!=-1) return dp[i][sum];
-        bool tk=(sum>=nums[i])?f(i-1,sum-nums[i],nums,dp):false;
-        bool nt=f(i-1,sum,nums,dp);
-
-        return dp[i][sum]=(tk||nt);
+        
+        bool tk=(sum>=arr[i])?f(i-1,sum-arr[i],arr,dp):0;
+        bool nt=f(i-1,sum,arr,dp);
+        
+        return dp[i][sum]=tk|| nt;
     }
-    bool canPartition(vector<int>& nums) {
-        int sum=0,n=nums.size();
-        for(auto x:nums){
+    bool canPartition(vector<int>& arr) {
+        int sum=0;
+        for(auto x:arr){
             sum+=x;
         }
-         if(sum%2) return false;
-        sum/=2;
-
-       vector<vector<int>>dp(n,vector<int>(sum+1,-1));
-         
-        return f(n-1,sum,nums,dp);
-
+        if(sum&1) return false;
+        sum=sum/2;
+        
+        int n=arr.size();
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        return f(n-1,sum,arr,dp);
     }
-
 };
