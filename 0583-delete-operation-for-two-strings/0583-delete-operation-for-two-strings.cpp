d3e68f73447cc5274894cc1c5fa330ecf028b33d
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-10 11:03:14 +0530

    Time: 19 ms (16.03%), Space: 16.6 MB (26.54%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-  int f(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
-    if(i<0 ||j<0) return 0;
-   if(dp[i][j]!=-1) return dp[i][j];
-    if(s1[i]==s2[j]){
-        return dp[i][j]=1+f(i-1,j-1,s1,s2,dp);
+    int f(int i ,int j,string &s1, string &s2,vector<vector<int>>&dp){
+        // if(i==0 && j==0) return 0;
+     if(i==0 || j==0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i-1]==s2[j-1]){
+            return 1+f(i-1,j-1,s1,s2,dp);
+        }
+        return  dp[i][j]=max(f(i,j-1,s1,s2,dp),f(i-1,j,s1,s2,dp));
     }
-
-    return dp[i][j]=max(f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp));
-   }
     int minDistance(string s1, string s2) {
-        int n=s1.size();
+         int n=s1.size();
         int m=s2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        int t1=f(n-1,m-1,s1,s2,dp);
-        return (n+m-2*t1);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        int ans=f(n,m,s1,s2,dp);
+        return s1.size()+s2.size()-2*ans;
     }
 };
