135af199d0f7227d0f29143d607b535d75865678
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-10 11:00:51 +0530

    Time: 66 ms (8.53%), Space: 30 MB (28.7%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int f(int i,int j,string &s1,string &s2,vector<vector<int>>&dp){
-    if(i<0 ||j<0) return 0;
-   if(dp[i][j]!=-1) return dp[i][j];
-    if(s1[i]==s2[j]){
-        return dp[i][j]=1+f(i-1,j-1,s1,s2,dp);
+     int f(int i ,int j,string &s1, string &s2,vector<vector<int>>&dp){
+        // if(i==0 && j==0) return 0;
+     if(i==0 || j==0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i-1]==s2[j-1]){
+            return 1+f(i-1,j-1,s1,s2,dp);
+        }
+        return  dp[i][j]=max(f(i,j-1,s1,s2,dp),f(i-1,j,s1,s2,dp));
     }
-
-    return dp[i][j]=max(f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp));
-   }
-    int minInsertions(string s) {
-        string t=s;
-         int n=s.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        reverse(t.begin(),t.end());
-        int te=f(n-1,n-1,s,t,dp);
-        return n-te;
+    int minInsertions(string s1) {
+          string s2=s1;
+          reverse(s2.begin(),s2.end());
+        int n=s1.size();
+        int m=s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        int ans=f(n,m,s1,s2,dp);
+        return s1.size()-ans;
     }
 };
