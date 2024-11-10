3e10834a7134542bf86307ffb2e4f6628e23d0bf
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-10 11:14:26 +0530

    Time: 11 ms (28.68%), Space: 13.6 MB (18.08%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int f(int i,int j,string &s1,string &s2, vector<vector<int>>&dp){
-        if(i==0) return j;
-        if(j==0) return i;
-       if(dp[i][j]!=-1) return dp[i][j];
+     int f(int i ,int j,string &s1, string &s2,vector<vector<int>>&dp){
+
+         if(i==0) return j;
+         if(j==0) return i;
+        if(dp[i][j]!=-1) return dp[i][j];
         if(s1[i-1]==s2[j-1]){
             return f(i-1,j-1,s1,s2,dp);
         }
-        int del=1+f(i-1,j,s1,s2,dp);
-        int ins=1+f(i,j-1,s1,s2,dp);
-        int rep=1+f(i-1,j-1,s1,s2,dp);
-
-        return dp[i][j]=min({del,ins,rep});
-    }
+        return  dp[i][j]=min({1+f(i,j-1,s1,s2,dp),1+f(i-1,j,s1,s2,dp),1+f(i-1,j-1,s1,s2,dp)});
+     }
     int minDistance(string s1, string s2) {
-        int n=s1.size();
+          int n=s1.size();
         int m=s2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return f(n,m,s1,s2,dp);
+        int ans=f(n,m,s1,s2,dp);
+        return ans;
     }
 };
