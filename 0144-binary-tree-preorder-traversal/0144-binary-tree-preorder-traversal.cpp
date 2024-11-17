dc1a6535e24e8b61e6754bac1c61d0f921367297
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-17 16:33:09 +0530

    Time: 0 ms (100%), Space: 10.1 MB (81.17%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,63 +11,19 @@
  */
 class Solution {
 public:
-    void f(vector<int>&v,TreeNode* root){
-        if(root==nullptr) return;
-        v.push_back(root->val);
-        f(v,root->left);
-                f(v,root->right);
+     void helper(TreeNode* root,vector<int>&ans){
+        if(!root) return;
+         ans.push_back(root->val);
+        helper(root->left,ans);
+       
+         helper(root->right,ans);
 
-    }
-    vector<int> preorderTraversal1(TreeNode* root) {
+     }
+   
+    vector<int> preorderTraversal(TreeNode* root) {
+         if(!root) return {};
         vector<int>ans;
-        // f(ans,root);
-        // return ans;
-        if(!root) return ans;
-        stack<TreeNode*>s;
-        s.push(root);
-        while(!s.empty()){
-            TreeNode *t=s.top();
-            ans.push_back(t->val);
-            s.pop();
-            if(t->right!=nullptr){
-                s.push(t->right);
-            }
-            if(t->left!=nullptr){
-                s.push(t->left);
-            }
-        }
-        return ans;
-    }
-
-    vector<int> preorderTraversal(TreeNode* root){
-        vector<int>ans;
-        TreeNode *cur=root;
-        while(cur!=NULL){
-            if(cur->left==NULL){
-                ans.push_back(cur->val);
-                cur=cur->right;
-            }
-            else{
-               TreeNode *prev=cur->left;
-               while(prev->right!=NULL && prev->right!=cur){
-                   prev=prev->right;
-               }
-
-               if(prev->right==NULL){
-                  prev->right=cur;
-                  ans.push_back(cur->val);
-                  cur=cur->left;
-               }
-               else{
-                prev->right=NULL;
-                cur=cur->right;
-               }
-
-
-
-
-            }
-        }
+        helper(root,ans);
         return ans;
     }
 };
