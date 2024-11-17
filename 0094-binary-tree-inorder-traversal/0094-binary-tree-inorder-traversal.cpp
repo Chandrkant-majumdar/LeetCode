0a1d16bd004a6a48ffe6ebd0767e1a61fc3bcaa6
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2024-11-17 16:31:55 +0530

    Time: 0 ms (100%), Space: 10.1 MB (38.6%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,72 +11,17 @@
  */
 class Solution {
 public:
+     void helper(TreeNode* root,vector<int>&ans){
+        if(!root) return;
+        helper(root->left,ans);
+        ans.push_back(root->val);
+         helper(root->right,ans);
 
-    void f(vector<int>&v,TreeNode*  root){
-        if(root==nullptr) return;
-       
-        f(v,root->left);
-         v.push_back(root->val);
-         f(v,root->right);
-    }
-    
-    vector<int> inorderTraversal1(TreeNode* root) {
-        vector<int>ans;
-        TreeNode *t=root;
-        while(t!=nullptr){
-            if(t->left==nullptr){
-                
-                ans.push_back(t->val);
-                t=t->right;
-            }else{
-                TreeNode *prev=t->left;
-                while(prev->right && prev->right!=t){
-                    prev=prev->right;
-                }
-
-                if(prev->right==nullptr){
-                    prev->right=t;
-                    t=t->left;
-                }else{
-                    prev->right=nullptr;
-                    ans.push_back(t->val);
-                    t=t->right;
-                }
-            }
-        }
-        // f(ans,root);
-        return ans;
-    }
+     }
     vector<int> inorderTraversal(TreeNode* root) {
-       vector<int>ans;
-       TreeNode *cur=root;
-       if(!root) return {};
-       while(cur!=NULL){
-        if(cur->left==NULL){
-            ans.push_back(cur->val);
-            cur=cur->right;
-        }
-        else{
-            TreeNode* prev=cur->left;
-            while(prev->right && prev->right!=cur){
-                prev=prev->right;
-               
-            }
-            
-            if(prev->right==NULL){
-                
-                 prev->right=cur;
-                 cur=cur->left;               
-
-            }else{
-                prev->right=NULL;
-                ans.push_back(cur->val);
-
-                cur=cur->right;
-            }
-           
-        }
-       }
-       return ans;
+        if(!root) return {};
+        vector<int>ans;
+        helper(root,ans);
+        return ans;
     }
 };
