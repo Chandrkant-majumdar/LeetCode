f4477f699d3c2e8b0d6eeb49ae5e6e8f382fa5de
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2025-03-25 20:34:32 +0530

    Time: 0 ms (100%), Space: 9.8 MB (79.03%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string s1="";
-        for(auto c:s){
-            if((c>='a' && c<='z') || (c>='0' && c<='9')) s1+=c;
-            else if(c>='A' && c<='Z') s1+=(c-'A')+'a';
+        int left = 0, right = s.size()-1;
+        while(left < right){
+            while(left < right && !isalnum(s[left]))
+                left++;
+            while(left < right && !isalnum(s[right]))
+                right--;
+            
+            if(tolower(s[left]) != tolower(s[right]))
+                return false;
+            left++;
+            right--;
         }
-        string s2=s1;
-        reverse(s2.begin(),s2.end());
-        return s1==s2;
+        return true;
     }
 };
