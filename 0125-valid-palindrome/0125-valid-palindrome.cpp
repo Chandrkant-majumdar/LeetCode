4f64652c119fcfdc8fe62bd730f1b93f5f9dc287
Chandrakant Majumdar <87277296+Chandrkant-majumdar@users.noreply.github.com>
2026-03-14 12:48:14 +0530

    Time: 3 ms (30.38%), Space: 10.7 MB (8.29%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0, right = s.size()-1;
-        while(left < right){
-            while(left < right && !isalnum(s[left]))
-                left++;
-            while(left < right && !isalnum(s[right]))
-                right--;
-            
-            if(tolower(s[left]) != tolower(s[right]))
-                return false;
-            left++;
-            right--;
+        string r="",t="";
+        // cout<<int('A')<<int('a');
+        for(auto c:s){
+            if(isalpha(c) || isdigit(c))
+            t+=tolower(c); 
         }
-        return true;
+        r=t;
+        
+        reverse(r.begin(),r.end());
+        cout<<r<<" "<<t;
+        return t==r;
     }
 };
